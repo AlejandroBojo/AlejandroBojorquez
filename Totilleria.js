62dc0a6013ae47610f9266a89d36cfbde61d1c52
AlejandroBojo <pedro_1321124130@uptecamac.edu.mx>
2021-09-21 19:48:48 -0500

    avance

---
+++ Totilleria.js
@@ -0,0 +1,20 @@
+var clima = "clima";
+var precio = "precio";
+var sol = "sol";
+var lluvia = "lluvia";
+var precio2 = "16";
+
+console.log("me mandan por las tortillas");
+console.log("me asomo si hace frio o hace calor");
+if ( clima == "lluvia") {
+    console.log ("me voy a poner un sueter");
+    } else {
+        console.log ("Hace calor me voy asi");
+    }
+console.log ("salgo por las tortillas");
+console.log ("llego a la tortilleria y verifico si me alcanza para un kilo");
+if ( precio == "precio2") {
+    console.log ("si me alcanzo y voy a comprar un kilo");
+} else {
+    console.log ("no me alcanzo para el kilo compro lo que me alcanze");
+}
