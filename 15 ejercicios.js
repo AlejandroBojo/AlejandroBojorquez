2d726ebae9cce096dc91334dd41d76e282a90a08
AlejandroBojo <pedro_1321124130@uptecamac.edu.mx>
2021-10-06 13:52:01 -0500

    Libreta acomodada

---
+++ 15 ejercicios.js
@@ -0,0 +1,10 @@
+var c = "si"
+var n = "no"
+var decision = "si"
+var decla = "no"
+//1
+if (decision) == si ) {
+    console.log ("voy por un hotdog");
+} else {
+    console.log("no voy porque hace frio");
+}
